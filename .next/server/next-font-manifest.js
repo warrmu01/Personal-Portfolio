f7bacc141d58415a8cfd8c8513544fedd53c7e5c
Mughees Warraich <warrmu01@luther.edu>
2024-08-15 22:42:49 -0500

    portfolio base

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/m.mughees/Desktop/Personal-Portfolio/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/162bf645eb375add-s.p.ttf\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
